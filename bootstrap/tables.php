690072af0307da6922a52272cd844309799f29fa
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-09-06 15:10:28 +1000

    Added Products structure

--- bootstrap/tables.php
+++ bootstrap/tables.php
@@ -1,2 +1,15 @@
 <?php
 
+// Products SQL Query
+$products = "CREATE TABLE IF NOT EXISTS products(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    cost float(100, 2) NOT NULL,
+    description longtext NOT NULL,
+    stock INT(5) NOT NULL DEFAULT 0,
+    published DATETIME
+)";
+
+if ($DB->query($products) !== TRUE) {
+    var_dump('Products table was NOT created');
+}
