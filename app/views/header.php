43258b18855877bafde7622c92da52fc0ee834f4
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-09-06 18:56:37 +1000

    Project Header and Footer, included bootstrap and jquery

---
+++ app/views/header.php
@@ -0,0 +1,33 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>PHP Commerce</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="/">PHP</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                    <div class="navbar-nav my-2 my-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/cart">
+                                Cart
+                                <span class="badge badge-pill badge-primary"><?php echo count($_SESSION['cart']); ?></span>
+                            </a>
+                        </li>
+                    </div>
+                </div>
+            </div>
+        </nav>
