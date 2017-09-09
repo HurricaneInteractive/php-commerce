6b1772030eb2e91a3d706628dcae9460682b3c6b
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-09-09 20:21:34 +1000

    Added basic checkout page structure

--- app/views/checkout/index.php
+++ app/views/checkout/index.php
@@ -5,4 +5,21 @@
     require '../app/views/header.php';
 ?>
 
-<h1>CHeckout Page</h1>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-sm-12">
+            <h2>Checkout</h2>
+        </div>
+        <div class="col-sm-12">
+            <table class="table table-striped">
+                <thead class="table-inverse">
+                    <tr class="bg-primary">
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Price ($)</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
