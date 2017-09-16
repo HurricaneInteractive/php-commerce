53c159d6f1c11ef73d1526e0a85c1303f59fd7e7
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-09-16 21:35:56 +1000

    Updated homepage structure

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -7,10 +7,25 @@
     $products = $data['products'];
 ?>
 
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-3">Welcome to PHP Commerce</h1>
+            <p class="lead">The is a prototype e-commerce site built with PHP and Stripe</p>
+        </div>
+    </div>
+
     <div class="container mt-3">
         <div class="row">
             <div class="col-md-12 mb-4">
-                <h1>Browse Products <small class="text-muted">Results (<?php echo count($products); ?>)</small></h1>
+                <div class="row">
+                    <nav class="w-100 navbar navbar-light justify-content-between">
+                        <a class="navbar-brand">Browse Products (<?php echo count($products); ?>)</a>
+                        <form class="form-inline">
+                            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </nav>
+                </div>
             </div>
             <?php
                 if (!empty($products)) {
@@ -21,12 +36,15 @@
                         $desc = $product->get_description();
                         $cost = $product->get_cost();
                         ?>
-                            <div class="col-sm-4 product">
-                                <h2><?php echo $name; ?></h2>
-                                <p><?php echo $desc ?></p>
-                                <small style="display: block;">$<?php echo $cost ?></small>
-                                <a data-id="<?php echo $id; ?>" href="/add-to-cart" class="btn btn-primary">Add to cart</a>
-                                <a href="/home/product?id=<?php echo $id; ?>" class="btn btn-link">View</a>
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h4 class="card-title"><?php echo $name; ?></h4>
+                                        <p class="card-text"><?php echo $desc ?></p>
+                                        <a data-id="<?php echo $id; ?>" href="/add-to-cart" id="add-to-cart" class="card-link">Add to cart</a>
+                                        <a href="/home/product?id=<?php echo $id; ?>" class="card-link">View</a>
+                                    </div>
+                                </div>
                             </div>
                         <?php
                     }
